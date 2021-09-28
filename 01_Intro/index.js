c5f57455cd95ad8e9a6311306015d1a472506daf
mikegomdi <mikegomez1493@gmail.com>
2021-09-27 20:13:44 -0500

    turf

--- 01_Intro/index.js
+++ 01_Intro/index.js
@@ -29,7 +29,29 @@ var geojson = [
     },
     properties: {
       name: "SFTM",
-      title: "SFTM",
+      title: "SFTM, GTO",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-100.92287185016805, 21.159840284922293],
+    },
+    properties: {
+      name: "DH",
+      title: "DG, GTO",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-101.85057448521778, 21.01738184450652],
+    },
+    properties: {
+      name: "DH",
+      title: "DG, GTO",
     },
   },
 ];
@@ -46,3 +68,44 @@ var puntos = L.geoJSON(geojson, {
 });
 
 mymap.addLayer(puntos);
+
+var pt1 = turf.point(coords[0]);
+var pt2 = turf.point(coords[1]);
+var pt3 = turf.point(coords[2]);
+console.log(pt1);
+
+var options = { units: "kilometers" };
+
+var distance = turf.distance(pt1, pt2, options);
+
+console.log(distance);
+
+distance = Math.round(distance);
+
+document.write(
+  "La distancia entre los dos puntos es de " + distance + " kilometros"
+);
+
+//Linea recta entre dos puntos
+// var linea = turf.lineString([coords[0], coords[1]]);
+// L.geoJSON(linea, { color: "blue" }).addTo(mymap);
+
+// var linea = turf.lineString([coords[0], coords[2]]);
+// L.geoJSON(linea, { color: "red" }).addTo(mymap);
+
+// var linea = turf.lineString([coords[1], coords[2]]);
+// L.geoJSON(linea, { color: "green" }).addTo(mymap);
+
+var features = turf.points(coords);
+
+// var center = turf.center(features);
+// console.log(center);
+// L.geoJSON(center).addTo(mymap);
+
+const polygon = turf.polygon([[...coords, coords[0]]]);
+
+var centroid = turf.centroid(polygon);
+
+L.geoJSON(polygon, { color: "red" }).addTo(mymap);
+
+L.geoJSON(centroid).addTo(mymap);
